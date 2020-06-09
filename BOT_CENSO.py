e7023208922b9aac9123fdf24f9761944ecb7173
diegoPaladino <diegopaladinofoto@gmail.com>
2020-06-09 16:10:49 -0300

    first commit

---
+++ BOT_CENSO.py
@@ -0,0 +1,12 @@
+#BOT_CENSO
+
+import pyautogui
+
+pyautogui.moveTo(x=624, y=-572)
+pyautogui.doubleClick()
+pyautogui.typewrite('P@ladino804680')
+pyautogui.PAUSE=1.5
+pyautogui.hotkey('enter')
+pyautogui.PAUSE=1
+
+

32e04775e7c3e3659262eeccca983fa9d5d3f173
diegoPaladino <diegopaladinofoto@gmail.com>
2020-06-18 14:24:29 -0300

    .

--- #ESCRITA_DATA_NASCIMENTO_CENSO.py
+++ #ESCRITA_DATA_NASCIMENTO_CENSO.py
@@ -1,15 +1,17 @@
 #ESCRITA_DATA_NASCIMENTO_CENSO
 
 import pyautogui
-
+import time
 
 # seleção Mozila para inserção da data
 pyautogui.moveTo(194, -23)
 pyautogui.click()
+time.sleep(1)
 
 # seleção text box data de nascimento
 pyautogui.moveTo(144, -110)
 pyautogui.click()
+time.sleep(1)
 
 # digitação da data de nascimento
 pyautogui.write('26/12/2008')

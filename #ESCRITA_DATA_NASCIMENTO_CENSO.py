7af55d7e9d7ff7a81a69d1a53c9abbe22ce6c5a6
diegoPaladino <diegopaladinofoto@gmail.com>
2020-06-18 19:26:57 -0300

    .

--- #ESCRITA_DATA_NASCIMENTO_CENSO.py
+++ #ESCRITA_DATA_NASCIMENTO_CENSO.py
@@ -14,10 +14,35 @@ pyautogui.click()
 time.sleep(0.3)
 
 # digitação da data de nascimento
-pyautogui.write('01/01/2009')
+pyautogui.write('03/06/2008')
 
 # mandar pesquisar no Censo
-pyautogui.press(['tab', 'tab', 'tab', 'tab', 'tab', ])
+pyautogui.press(['tab', 'tab', 'tab', 'tab', 'tab'])
 pyautogui.hotkey('enter')
 time.sleep(2)
 pyautogui.press(['pagedown', 'pagedown'])
+
+# procura pelo nome da mae
+pyautogui.moveTo(242, -26, 0.5)
+pyautogui.click()
+pyautogui.hotkey('esc')
+pyautogui.hotkey('left')
+# copia de nome da mãe na planilha do Excel
+pyautogui.moveTo(134, -722)
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.dragTo(490, -722, 0.5, button='left')       #deu certo este daqui
+pyautogui.hotkey('ctrl','c')
+
+# seleção Mozila para inserção do nome da mãe
+pyautogui.moveTo(194, -23)
+pyautogui.click()
+time.sleep(0.3)
+
+# inserção do nome da mãe no campo de pesquisa
+pyautogui.keyDown('ctrl')
+pyautogui.hotkey('f')
+pyautogui.keyUp('ctrl')
+pyautogui.keyDown('ctrl')
+pyautogui.hotkey('v')
+pyautogui.keyUp('ctrl')

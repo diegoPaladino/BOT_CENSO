39ef06d3e9b791e9a4083b968e0f1386a2b5cc59
diegoPaladino <diegopaladinofoto@gmail.com>
2020-06-19 19:39:17 -0300

    .

--- #aluno_sem_registo.py
+++ #aluno_sem_registo.py
@@ -12,3 +12,37 @@ time.sleep(0.3)
 pyautogui.press('left')
 time.sleep(0.3)
 
+# selecionando a palheta de cores
+pyautogui.moveTo(242, -784)
+pyautogui.click()
+time.sleep(0.3)
+
+# selecionando a cor amarela/alaranjado
+pyautogui.moveTo(261, -628)
+pyautogui.click()
+time.sleep(0.3)
+
+# voltando para a cor verde
+pyautogui.press('up')
+time.sleep(0.2)
+
+# selecionando a palheta de cores
+pyautogui.moveTo(242, -784)
+pyautogui.click()
+time.sleep(0.3)
+
+# selecionando a cor verde
+pyautogui.moveTo(379, -737)
+pyautogui.click()
+time.sleep(0.3)
+
+# selecionando próximo nome
+pyautogui.press(['down', 'down'])
+
+# VSCODE - deixando janela do VSCode selecionada
+pyautogui.moveTo(289, -23)
+pyautogui.click()
+
+# VSCODE - selecionando aba do software 1
+pyautogui.moveTo(457, 45)
+pyautogui.click()

dfaea856b12a1cb78971417fc55d8d963b789161
diegoPaladino <diegopaladinofoto@gmail.com>
2020-06-26 09:31:28 -0300

    .

--- #Transferidos_conferencia.py
+++ #Transferidos_conferencia.py
@@ -22,7 +22,7 @@ pyautogui.moveTo(139, -21)
 pyautogui.click()
 time.sleep(0.2)
     # selecionando barra de Nº de matrícula
-pyautogui.moveTo(139, -21)
+pyautogui.moveTo(301, 378)
 pyautogui.click()
 time.sleep(0.2)
     # colando Nº de matrícula
@@ -30,5 +30,23 @@ pyautogui.keyDown('ctrl')
 pyautogui.hotkey('v')
 pyautogui.keyUp('ctrl')
 pyautogui.hotkey('tab')
+time.sleep(1.5)
+
+# movendo para dados / "ocorrencias"
+pyautogui.moveTo(682, 324)
+pyautogui.click()
+time.sleep(0.2)
+
+# EXCEL - Copiando dados para carregar na plataforma do Censo
+pyautogui.moveTo(242, -15)
+pyautogui.click()
+time.sleep(0.2)
+pyautogui.hotkey('right')
+    # copiando nome do aluno
+pyautogui.
 
 
+# CENSO - carregando dados na plataforma Censo
+pyautogui.moveTo(191, -25)
+pyautogui.click()
+time.sleep(0.2)
